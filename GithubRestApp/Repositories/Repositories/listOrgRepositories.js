55dc1cc8197d6047f71debed90ad6e4686c6fa3a
Rodrigo Motta <rmotta.net@gmail.com>
2024-11-29 11:55:19 -0300

    Retrieves the repositories of a specific Organization on GitHub

---
+++ GithubRestApp/Repositories/Repositories/listOrgRepositories.js
@@ -0,0 +1,32 @@
+/**
+ * Retrieves the repositories of a specific Organization on GitHub.
+ *
+ * This function makes a request to the GitHub API to retrieve the repository data of a specific organization, using the personal access token provided.
+ *
+ * **Important:** This function is specific to Google Apps Script and uses the `Lab_GithubHttpApp.fetcher` library to make the HTTP requests.
+ *
+ * @async
+ * @param {string} org - The GitHub Organization name for which the repositories will be retrieved.
+ * @param {string} GH_TOKEN - GitHub's personal access token for authentication.
+ * @returns {Promise<any>} A Promise that resolves with an array of objects representing the organization's repositories, or null in the event of an error.
+ */
+async function listOrganizationRepositories(org, GH_TOKEN) {
+
+  try {
+    
+    const endpoint = GH_API + `/orgs/${org}/repos`
+    const repoData = await Lab_GithubHttpApp.fetcher(GH_TOKEN, endpoint);
+
+    if (!repoData) {
+      Logger.log('Error retrieving repositories.');
+      return null;
+    }
+    
+    return repoData;
+    
+  } catch (error) {
+    Logger.log('listOrganizationRepositories error: ' + error.message);
+    return null;
+  }
+  
+}
